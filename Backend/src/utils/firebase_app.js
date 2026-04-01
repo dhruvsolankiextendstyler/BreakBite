1e340d4f49b56c51c64ebd6d9416896e2334404f
tyro-codes <dhruvds1971@gmail.com>
2026-04-01 17:13:28 +0530

    setup: added env, fixed firebase path, added start script

--- Backend/src/utils/firebase_app.js
+++ Backend/src/utils/firebase_app.js
@@ -1,6 +1,5 @@
 import firebase from "firebase-admin"
-import serviceAccount from "../../serviceAccount.json" with {type:'json'}
-
+import serviceAccount from "./serviceAccount.json" with {type:'json'}
 export const admin = firebase.initializeApp({
     credential: firebase.credential.cert(serviceAccount)
 })
